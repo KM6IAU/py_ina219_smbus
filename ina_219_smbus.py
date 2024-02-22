8300799cb78350c60c533a9d108ab4a7e6ee0850
Aaron KM6IAU <Aaron@KM6IAU.net>
2024-02-21 20:24:33 -0800

    first commit

--- ina_219_smbus.py
+++ ina_219_smbus.py
@@ -1,6 +1,6 @@
 import smbus2
 class ina219_i2c():
-	def __init__(self,_BUS=0,cal=4096):
+	def __init__(self,_BUS=1,cal=4096):
 		self.INA219_ADDRESS=0x40
 		self.INA219_CONFIG=0x00
 		self.INA219_SV=0x01
@@ -12,17 +12,17 @@ class ina219_i2c():
 
 		#config register configurations
 		self.INA219_CONFIG_RESET=(0x8000)  # Reset Bit
-	
+
 		self.INA219_CONFIG_BVOLTAGERANGE_MASK=(0x2000)  # Bus Voltage Range Mask
 		self.INA219_CONFIG_BVOLTAGERANGE_16V=(0x0000)  # 0-16V Range
 		self.INA219_CONFIG_BVOLTAGERANGE_32V=(0x2000)  # 0-32V Range
-			
+
 		self.INA219_CONFIG_GAIN_MASK=(0x1800)  # Gain Mask
 		self.INA219_CONFIG_GAIN_1_40MV=(0x0000)  # Gain 1, 40mV Range
 		self.INA219_CONFIG_GAIN_2_80MV=(0x0800)  # Gain 2, 80mV Range
 		self.INA219_CONFIG_GAIN_4_160MV=(0x1000)  # Gain 4, 160mV Range
 		self.INA219_CONFIG_GAIN_8_320MV=(0x1800)  # Gain 8, 320mV Range
-			
+
 		self.INA219_CONFIG_BADCRES_MASK=(0x0780)  # Bus ADC Resolution Mask
 		self.INA219_CONFIG_BADCRES_9BIT=(0x0080)  # 9-bit bus res = 0..511
 		self.INA219_CONFIG_BADCRES_10BIT=(0x0100)  # 10-bit bus res = 0..1023
@@ -41,7 +41,7 @@ class ina219_i2c():
 		self.INA219_CONFIG_SADCRES_12BIT_32S_17MS=(0x0068)  # 32 x 12-bit shunt samples averaged together
 		self.INA219_CONFIG_SADCRES_12BIT_64S_34MS=(0x0070)  # 64 x 12-bit shunt samples averaged together
 		self.INA219_CONFIG_SADCRES_12BIT_128S_69MS=(0x0078)  # 128 x 12-bit shunt samples averaged together
-			
+
 		self.INA219_CONFIG_MODE_MASK=(0x0007)  # Operating Mode Mask
 		self.INA219_CONFIG_MODE_POWERDOWN=(0x0000)
 		self.INA219_CONFIG_MODE_SVOLT_TRIGGERED=(0x0001)
@@ -50,7 +50,7 @@ class ina219_i2c():
 		self.INA219_CONFIG_MODE_ADCOFF=(0x0004)
 		self.INA219_CONFIG_MODE_SVOLT_CONTINUOUS=(0x0005)
 		self.INA219_CONFIG_MODE_BVOLT_CONTINUOUS=(0x0006)
-		self.INA219_CONFIG_MODE_SANDBVOLT_CONTINUOUS=(0x0007) 
+		self.INA219_CONFIG_MODE_SANDBVOLT_CONTINUOUS=(0x0007)
 		#self._i2c_addr=_INA219_ADDRESS
 		self._CAL=cal
 		self.bus=smbus2.SMBus(_BUS)
@@ -59,10 +59,10 @@ class ina219_i2c():
 	def set_config(self,config_list):
 		cfg=sum(config_list)
 		self.write_word(self.INA219_CONFIG,cfg)
-	
+
 	def reset(self,):
 		self.write_word(self.INA219_CONFIG,self.INA219_CONFIG_RESET)
-	
+
 	def set_gain(self,gain):
 		config=self.read_word(self.INA219_CONFIG)
 		config=config & 0b1110011111111111
@@ -104,15 +104,15 @@ class ina219_i2c():
 	def get_shunt_voltage(self,mV=False):
 		raw_value=self.read_word(self.INA219_SV)
 		return raw_value
-	
+
 	def get_current(self,mA=True):
 		self.set_cal(self.s_cal)
 		raw_value=self.read_word(self.INA219_CU)
-                if raw_value > 32767:
-                        raw_value = raw_value - 65535
+		if raw_value > 32767:
+			raw_value = raw_value - 65535
 		current=(raw_value/10)
 		return round(current/(1000.0 if mA==False else 1.0),3)
-	
+
 	def get_power(self,mW=True):
 		'''
 		raw_value=self.read_word(self.INA219_PW)
@@ -128,11 +128,10 @@ class ina219_i2c():
 		ac_high_byte=read_data & 0xFF
 		ac_low_byte=(read_data & 0xFF00)>>8
 		return (ac_high_byte<<8)+ac_low_byte
-		
+
 	def write_word(self,address,data):
 		cnv_high_byte=data & 0xFF
 		cnv_low_byte=(data & 0xFF00)>>8
 		formed_data=(cnv_high_byte<<8)+cnv_low_byte
 		self.bus.write_word_data(self.INA219_ADDRESS,address,formed_data)
-		
 
--- test.py
+++ test.py
@@ -1,9 +1,42 @@
+#!/usr/bin/python3
+
+
+
+
+
 import time
+
 from ina_219_smbus import ina219_i2c
+
+
 
 ina219=ina219_i2c()
 
+
+
 #ina219.set_cal(4096)
+
 while(True):
-	print(ina219.get_bus_voltage(),ina219.get_current(mA=False),ina219.get_power(mW=False))
+
+	mV = False
+
+	raw_value = ina219.get_shunt_voltage()
+
+	shunt_voltage = round((raw_value>>3)*4*(0.001 if mV==False else 1),2)
+
+
+
+	print(
+
+		"bus V=", ina219.get_bus_voltage(mV=False),
+
+		"sh V=", shunt_voltage,
+
+		"A=", ina219.get_current(mA=False),
+
+		"W=", ina219.get_power(mW=False)
+
+	)
+
 	time.sleep(1)
+
